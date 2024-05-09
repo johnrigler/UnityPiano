f68f569a1327b1f46e2b07e8ec3f8bf320ab4f2b
John Rigler <jrigler@axisdatalytics.com>
2024-05-08 23:33:33 -0500

    refactored

--- Assets/Keyboard.cs
+++ Assets/Keyboard.cs
@@ -3,61 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// public class Keyboard : MonoBehaviour
 public class Keyboard : MonoBehaviour
 {
     public GameObject blackTile, whiteTile; 
     public GameObject content;
+    private int startNote = 24;
+    private int[] blackKeyIndex = { 1, 3, 6, 8, 10 };
+    private int[] whiteKeyIndex= { 0, 2, 4, 5, 7, 9, 11 };
 
     public int numberOfOctaves;
     // Start is called before the first frame update
     void Start()
     {
-        int startNote = 24;
-        for (int i = 0; i < numberOfOctaves; i++)
-        {
-            createOctave(startNote+i*12, i);
-        }
+        for (int i = 0; i < numberOfOctaves; i++)createOctave(i);
     }
 
-
-    private void createOctave(int startNote, int octave){
+    private void createOctave(int octave){
         float width = content.GetComponent<RectTransform>().rect.width;
         float widthPerOctave = width / numberOfOctaves; 
-        float widthPerNote = widthPerOctave / 7; 
+        float widthPerNote = widthPerOctave / 7;
+
+        int myStartNote = (octave * 12) + startNote;
 
         // 7 white tiles
         for (int i = 0; i < 7; i++)
         {
-            int actualNoteIndex = getWhiteKeyIndex(i); 
-            GameObject note = instantiateNote(whiteTile, actualNoteIndex, startNote);
+            GameObject note = instantiateNote(whiteTile, whiteKeyIndex[i], myStartNote);
             registerEvents(note);
+           
+            RectTransform rT = note.GetComponent<RectTransform>();
+            RectTransform wT = whiteTile.GetComponent<RectTransform>();
 
-            note.GetComponent<RectTransform>().sizeDelta = new Vector2(widthPerNote-1, note.GetComponent<RectTransform>().sizeDelta.y); 
-            note.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(widthPerOctave*octave+widthPerNote*i + widthPerNote/2, -whiteTile.GetComponent<RectTransform>().rect.height/2,0);
-    
-        }
+            rT.sizeDelta = new Vector2(widthPerNote - 2, rT.sizeDelta.y);
+            rT.anchoredPosition3D = new Vector3(widthPerOctave * octave + widthPerNote * i + widthPerNote / 2, - wT.rect.height / 2, 0);
+        } 
 
         // 5 black tiles 
         for (int i = 0; i < 5; i++)
         {
-            int actualNoteIndex = getBlackKeyIndex(i); 
-            GameObject note = instantiateNote(blackTile, actualNoteIndex, startNote); 
+            GameObject note = instantiateNote(blackTile, blackKeyIndex[i], myStartNote); 
             registerEvents(note);
 
-            note.GetComponent<RectTransform>().sizeDelta = new Vector2(widthPerNote/2, note.GetComponent<RectTransform>().sizeDelta.y);
+            RectTransform rT = note.GetComponent<RectTransform>();
+            RectTransform bT = blackTile.GetComponent<RectTransform>();
+
+            rT.sizeDelta = new Vector2(widthPerNote/2, rT.sizeDelta.y);
 
             int blackIndex = i; 
-            if(i > 1){
-                blackIndex += 1; 
-            }
-            note.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(widthPerOctave*octave+widthPerNote*blackIndex+widthPerNote, -blackTile.GetComponent<RectTransform>().rect.height/2, 0);
+            if(i > 1){ blackIndex += 1; }
+            rT.anchoredPosition3D = new Vector3(widthPerOctave*octave+widthPerNote*blackIndex+widthPerNote, - bT.rect.height/2, 0);
         }
-        
-        
-        
-
     }
-
     private void registerEvents(GameObject note){
         EventTrigger trigger = note.gameObject.AddComponent<EventTrigger>(); 
         var pointerDown = new EventTrigger.Entry(); 
@@ -81,66 +78,10 @@ public class Keyboard : MonoBehaviour
         GameObject.Find("SoundGen").GetComponent<SoundGen>().onKeyOff(midiNumber);
     }
 
-
-
     private GameObject instantiateNote(GameObject note, int actualNoteIndex, int startNote){
         GameObject newNote = Instantiate(note);
         newNote.transform.SetParent(content.transform, false);
         newNote.GetComponent<PianoTile>().midiNote = startNote + actualNoteIndex;
         return newNote;
-    }
-
-    private int getWhiteKeyIndex(int i){
-        // C
-        int actualNoteIndex = 0;
-
-        if(i == 1){
-            // D
-            actualNoteIndex = 2;
-        } else if(i == 2){
-            // E
-            actualNoteIndex = 4; 
-        } else if(i == 3){
-            //F
-            actualNoteIndex = 5; 
-        }else if(i == 4){
-            //G
-            actualNoteIndex = 7; 
-        }
-        else if(i == 5){
-            //A
-            actualNoteIndex = 9; 
-        }
-        else if(i == 6){
-            //A
-            actualNoteIndex = 9; 
-        }
-        return actualNoteIndex;
-
-    }
-
-    private int getBlackKeyIndex(int i){
-        // CS
-        int actualNote = 1; 
-        if(i == 1){
-            // DS
-            actualNote = 3; 
-        } else if(i == 2){
-            // ES
-            actualNote = 6; 
-        } else if(i == 3){
-            //FS
-            actualNote = 8; 
-        }
-        return actualNote;
-    }
-
-    
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
